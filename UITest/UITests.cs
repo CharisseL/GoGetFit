25271a448032d6d99905627707c11faa2962f1d9
CharisseL <charisse8@gmail.com>
2015-02-01 02:00:55 -0600

    MainWindow set

---
+++ UITest/UITests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace UITests
+{
+    [TestClass]
+    public class UITests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

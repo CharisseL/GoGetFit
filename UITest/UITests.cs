d9056c6c5906788271d3a1edfdf9318ba7196b46
CharisseL <charisse8@gmail.com>
2015-02-04 03:24:19 -0600

    ZeroState test passed

--- UITest/UITests.cs
+++ UITest/UITests.cs
@@ -6,14 +6,38 @@ using TestStack.White.Factory;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.WindowItems;
 
-namespace UITests
+namespace GoGetFitTests
 {
     [TestClass]
-    public class UITests
+    public class UITests 
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static TestContext test_context;
+        private static Window window;
+        private static Application application;
+
+        [ClassInitialize]
+        public static void Setup(TestContext _context)
         {
+            test_context = _context;
+            var applicationDir = _context.DeploymentDirectory;
+            var applicationPath = Path.Combine(applicationDir, "..\\..\\..\\GoGetFit\\bin\\Debug\\GoGetFit");
+            application = Application.Launch(applicationPath);
+            window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+        }        
+
+        [TestMethod]
+        public void TestZeroState()
+        {
+            Button AddWorkoutButton = window.Get<Button>("AddWorkoutButton");
+
+            Assert.IsTrue(AddWorkoutButton.Enabled);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            window.Close();
+            application.Close();
         }
     }
 }

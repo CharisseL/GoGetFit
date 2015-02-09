373411101294b976a89ec9e3afadd29a0c2023d4
CharisseL <charisse8@gmail.com>
2015-02-09 15:05:12 -0600

    WorkoutCreation test

---
+++ UITest/StoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.BDDfy;
+
+namespace GoGetFitTests
+{
+    [TestClass]
+    public class StoryTests : TestHelper
+    {
+        [TestMethod]
+        public void WorkoutCreation()
+        {
+            //As a user
+            //In order keep track of my workouts
+            //I want to create a new workout
+            //Click "Add Workout" to get the add new workout form
+            //Workout must contain at least one character
+            //Event is required
+            //Click "Add" to add the workout
+            //The event shows up in the workout grid list and the form goes away
+            //Click "x" to close the form
+
+            GivenThereAreNoWorkouts();
+            WhenIClick("Add Workout");
+            ThenIShouldSeeWorkoutForm();
+            AndTheButtonShouldBeDisabled("Add Workout");
+
+            WhenIFillInWorkoutWith("Blue Trail Run");
+            AndIChooseTheWorkoutDate("02/14/15");
+            AndIClick("Add");
+            ThenIShouldNotSeeWorkoutForm();
+            AndIShouldSeeXWorkouts(1);
+        }
+
+    }
+}
---
+++ UITest/TestHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GoGetFitTests
+{
+    public class TestHelper
+    {
+
+        public void AndIShouldSeeXWorkouts(int p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThenIShouldNotSeeWorkoutForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AndIClick(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AndIChooseTheWorkoutDate(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WhenIFillInWorkoutWith(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AndTheButtonShouldBeDisabled(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ThenIShouldSeeWorkoutForm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WhenIClick(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GivenThereAreNoWorkouts()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
